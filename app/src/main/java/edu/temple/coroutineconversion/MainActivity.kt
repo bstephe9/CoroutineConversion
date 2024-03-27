efedef7e9ee4f20e53f679df6969a83b1f46126c
Ben Stephenson <tun78906@temple.edu>
2024-03-27 15:46:07 -0400

    Fix scope outside of onClickListener

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -9,7 +9,9 @@ import android.widget.ImageView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,19 +31,17 @@ class MainActivity : AppCompatActivity() {
         cakeImageView = findViewById(R.id.imageView)
 
         val scope = CoroutineScope(Job() + Dispatchers.Default)
-        scope.launch {
-            sendEmptyMessages()
-        }
-    }
 
-    suspend fun sendEmptyMessages() {
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+            scope.launch {
                 repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+                    withContext(Dispatchers.Main) {
+                        cakeImageView.alpha = it / 100f
+                    }
+                    delay(40)
                 }
-            }.start()
+            }
+
         }
     }
 }
